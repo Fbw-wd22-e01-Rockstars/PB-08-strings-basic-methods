016a7e59669c4e0a66e4435c75eeb01ca4fed462
nrcool <nomanalam7214@gmail.com>
2022-04-21 15:47:59 +0200

    solution

---
+++ index.js
@@ -0,0 +1,51 @@
+// 1. Declare a variable with the value of "I can walk in the park all day!". Print the word "park" to the console.
+const walk = "I can walk in the park all day!"
+
+console.log(walk.slice(18,-9));
+console.log(walk.substring(18,22))
+console.log(walk.substr(18,4))
+
+// 2. Declare a variable with the value of "Hello World". Covert the value to upper case and print the converted value to the console.
+const hello = "Hello World";
+console.log(hello.toUpperCase())
+
+// 3. Declare another variable with the value of "Hello Earthling". Convert the value to lower case and print the converted value to the console. 
+
+const earth= "Hello Earthling";
+console.log(earth.toLowerCase())
+
+// 4. Declare a variable with the value "JavaScript". Use string methods to print the the characters "aSc" from your declared variable.
+const language = "JavaScript";
+console.log(language.slice(3,6));
+
+// 5. Check if the sentence "nice shoes" contains the letter l or n. 
+
+ console.log("nice shoes".includes("n") || "nice shoes".includes("l") )
+
+// 6. Create a new string from a given string with the first character of the given string added at the front and back, i.e., expected output: Bananas => BBananasB
+
+const fruit = "Bananas";
+console.log(fruit.charAt(0)+ fruit + fruit.charAt(0))
+
+// 7. Create a new string from a given string taking the last 3 characters of the string and add them to both the front and back of the new string. The string length must be 3 or more, i.e., expected output: Scritch => tchScritchtch
+
+const anything="Scritch";
+console.log(anything.slice(-3)+ anything + anything.slice(-3) )
+
+
+// 8. Create a new string from a given string by changing the position of first and last characters. The string length must be greater than or equal to 2, i.e., expected output: BoogieWoogie => eoogieWoogiB
+
+const BW = "BoogieWoogie";
+
+console.log(BW.slice(-1)+ BW.slice(1,-1)+ BW.charAt(0) )
+
+// 9. Create 3 different variables about yourself, using strings as values, i.e, let firstName = "Maria"; let city = "Berlin", etc. Print a sentence to the console using the variables and string interpolation, i.e., "My name is Maria. I live in Berlin and I am a teacher". 
+const first_name = "Maria";
+const age = 24;
+const profession = "Teacher";
+ console.log(`My name is ${first_name}, I an ${age} year old and I am a ${profession}`) 
+
+// 10. Declare a variable and assign the value "the quick brown fox" (all in lower case). Capitalize the first letter of that string. Print the result to the console.
+
+const fox = "the quick brown fox";
+console.log(fox.charAt(0).toUpperCase() + fox.slice(1 ));
